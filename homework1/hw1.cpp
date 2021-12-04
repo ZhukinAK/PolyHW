e094a4d5038b55170af5abc39695d82a3972d8b0
LitvinovaYaI <90526520+LitvinovaYaI@users.noreply.github.com>
2021-12-04 14:16:59 +0300

    Update hw1.cpp

--- homework1/hw1.cpp
+++ homework1/hw1.cpp
@@ -1,73 +1,101 @@
-#include <iostream> 
+#include <iostream>
 #include <cmath>
 #include <fstream>
 #include <string>
 #include <cstring>
 using namespace std;
-double coordx(string str) {
-	int determinator = str.find(" ");
-	string X = str.substr(0, determinator);
-	double x = stoi(X);
+
+int position(double x0, double y0, double x, double y)
+{
+	if (x0 * y - y0 * x > 0)
+	{
+		return 0;   //лево
+	}
+	else
+	{
+		return 1;//право
+	}
+}
+
+double take_x(string str) {
+	int probel = str.find(" ");
+	double x = stoi(str.substr(0, probel));
 	return x;
 }
-double coordy(string str) {
-	int determinator = str.find(" ");
-	string Y = str.substr(determinator + 1, str.length());
-	double y = stoi(Y);
+
+double take_y(string str) {
+	int probel = str.find(" ");
+	double y = stoi(str.substr(probel+1, str.length()));
 	return y;
 	return 0;
 }
+
+float cosvec(double x0, double y0, double x, double y)
+{
+	float cosvector = (x * x0 + y0 * y) / sqrt((x0 * x0 + y0 * y0) * (x * x + y * y));
+	return cosvector;
+}
+
 int main()
 {
 	ifstream i_file;
 	i_file.open("in.txt");
+
 	int a = 0;
+
 	double x0;
 	double y0;
+
 	double x;
 	double y;
-	double mleftx;
-	double mlefty;
-	double mrightx;
-	double mrighty;
-	double dl = 0;
-	double dr = 0;
-	double dlina;
+
+	double leftx;
+	double lefty;
+
+	double rightx;
+	double righty;
+
+	double csl=1;
+	double csr = 1;
+
 	string str;
+
 	while (getline(i_file, str))
 	{
 		if (a == 0)
 		{
-			x0 = coordx(str);
-			y0 = coordy(str);
+			x0=take_x(str);
+			y0=take_y(str);
 			a = 1;
 		}
 		else
 		{
-			x = coordx(str);
-			y = coordy(str);
-			dlina = abs((x0 * y - y0 * x) / sqrt(y0 * y0 + x0 * x0));
-			if (x0 * y - y0 * x > 0)
+			x = take_x(str);
+			y = take_y(str);
+			int pos = position(x0, y0, x, y);
+			if (pos == 0)
 			{
-				if (dl <= dlina)
+				
+				if (csl >= cosvec(x0,y0,x,y))
 				{
-					dl = dlina;
-					mleftx = x;
-					mlefty = y;
+					csl = cosvec(x0, y0,x,y);
+					leftx = x;
+					lefty = y;
 				}
 			}
 			else
 			{
-				if (dr <= dlina)
+				
+				if (csr >= cosvec(x0, y0, x, y))
 				{
-					dr = dlina;
-					mrightx = x;
-					mrighty = y;
+					csr = cosvec(x0, y0, x, y);
+					rightx = x;
+					righty = y;
 				}
 			}
 		}
 	}
-	cout << "Leftmost: " << mleftx << " " << mlefty << endl;
-	cout << "Rightmost: " << mrightx << " " << mrighty << endl;
+	cout << "Leftmost: " << leftx << " " << lefty << endl;
+	cout << "Rightmost: " << rightx << " " << righty << endl;
 	return 0;
 }
